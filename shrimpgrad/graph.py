7c082cfe7dc89e2dfb9101ea759ee12e72021320
Kevin Kenyon <kvkenyon@gmail.com>
2023-12-14 22:55:43 -0600

    Add exp and cleanup
    Fix pylint stuff (will need a refactor to avoid some silly getter stuff)
    Support ops on int/float, add exp

--- shrimpgrad/graph.py
+++ shrimpgrad/graph.py
@@ -1,25 +1,41 @@
+"""
+Module provides visualization for the computational graph
+represented by a DAG of Value objects.
+"""
 import graphviz
 
+
 def build_graph(root):
+    """Builds a graphviz image of the comp DAG
+
+    Args:
+        root (Value): the DAG root
+
+    Returns:
+        Digraph: a graphviz Digraph object
+    """
     g = graphviz.Digraph('Computational Graph', node_attr={'shape': 'record'})
-    
+
     g.graph_attr['rankdir'] = 'LR'
+
     def recur(node):
         if not node:
             return
-        parents = node._prev
+        parents = node.prev
         if not parents:
             return
         child_label = node.label
-        child_op = node._op
+        child_op = node.get_op()
         child_data = node.data
         child_grad = node.grad
         op_label = f'{child_op}_{child_label}'
-        g.node(child_label, f'{child_label}| data {child_data:.4f} | grad {child_grad:.4f} ')
+        g.node(
+            child_label, f'{child_label}| data {child_data:.4f} | grad {child_grad:.4f} ')
         g.node(op_label, f'{child_op}', shape='circle')
         g.edge(op_label, child_label)
         for p in parents:
-            g.node(p.label, f'{p.label} | data {p.data:.4f} | grad {p.grad:.4f}')
+            g.node(
+                p.label, f'{p.label} | data {p.data:.4f} | grad {p.grad:.4f}')
             g.edge(p.label, op_label)
             recur(p)
     recur(root)
--- shrimpgrad/value.py
+++ shrimpgrad/value.py
@@ -1,18 +1,25 @@
+"""
+Module providing a scalar Value object 
+that supports operations with reverse automatic differentiation
+"""
 import math
 
 
 class Value:
+    """Class representing a scalar value with autograd"""
+
     def __init__(self, data, _children=(), _op='', label='', grad=0.0):
         # Public
         self.data = data
         self.grad = grad
         self.label = label
+        self.prev = list(_children)
         # Private
-        self._prev = list(_children)
         self._op = _op
         self._backward = lambda: None
 
     def backward(self):
+        """Method that executes backpropagation starting at self."""
         self.grad = 1.0
         visited = set()
         topo = []
@@ -20,14 +27,40 @@ class Value:
         def build_topo(v):
             if v not in visited:
                 visited.add(v)
-                for c in v._prev:
+                for c in v.prev:
                     build_topo(c)
                 topo.append(v)
         build_topo(self)
         for v in reversed(topo):
-            v._backward()
+            v.backward_intern()
+
+    def backward_intern(self):
+        """Get the internal backward function
+        implementing the backward pass for the op.
+
+        Returns:
+            Function: the backwards function
+        """
+        return self._backward
+
+    def set_backward_intern(self, bw):
+        """Set the internal backward pass for the op.
+
+        Args:
+            bw (Function): the backwards function
+        """
+        self._backward = bw
+
+    def get_op(self):
+        """Return the op used to evaluate this Value object
+
+        Returns:
+            string: the op as a string (i.e. '+', '*', 'tanh', etc.)
+        """
+        return self._op
 
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backward():
@@ -37,6 +70,7 @@ class Value:
         return out
 
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
@@ -45,19 +79,43 @@ class Value:
         out._backward = _backward
         return out
 
+    def __rmul__(self, other):
+        return self * other
+
     def tanh(self):
+        """Computes hyperbolic tangent (tanh) for the value.
+
+        Returns:
+            Value: the value after computing tanh
+        """
         x = self.data
         t = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
         out = Value(t, (self,), 'tanh')
 
         def _backward():
             self.grad += (1 - t**2) * out.grad
-        out._backward = _backward
+        out. set_backward_intern(_backward)
+        return out
+
+    def exp(self):
+        """Computes the exponential function for the value.
+
+        Returns:
+            Value: the value after e^x
+        """
+        x = self.data
+        out = Value(math.exp(x), (self,), 'exp')
+
+        def _backward():
+            self.grad = math.exp(x) * out.grad
+        out. set_backward_intern(_backward)
         return out
 
     def clear(self):
+        """Clear all the gradients in the graph.
+        """
         self.grad = 0.0
-        for c in self._prev:
+        for c in self.prev:
             c.clear()
 
     def __repr__(self):
