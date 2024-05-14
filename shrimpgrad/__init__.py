2f95cd18bb8bc2a4b5ee5f6a9b08e06225afa668
Kevin Kenyon <kvkenyon@gmail.com>
2024-05-13 21:15:13 -0500

    Remove cblas

--- shrimpgrad/__init__.py
+++ shrimpgrad/__init__.py
@@ -2,6 +2,5 @@ from shrimpgrad.scalar.value import *
 from shrimpgrad.scalar.graph import *
 from shrimpgrad.scalar.mlp import *
 from shrimpgrad.tensor import * 
-from shrimpgrad.cblas import *
 from shrimpgrad.autograd import *
 from shrimpgrad.runtime import python
