6a39e47d9a4375b24598ed40bf96799b58e10a14
Kevin Kenyon <kvkenyon@gmail.com>
2024-05-20 19:36:18 -0500

    Add future tensor changes to functions

--- shrimpgrad/autograd/function.py
+++ shrimpgrad/autograd/function.py
@@ -1,10 +1,8 @@
-import functools
 import math
 from typing import Any, Optional, Tuple, TypeAlias 
 import shrimpgrad as shrimp
-from shrimpgrad.dtype import dtypes
 from shrimpgrad.runtime.ops import UnaryOps, BinaryOps, TernaryOps, ReduceOps
-from shrimpgrad.util import flatten, prod, argsort
+from shrimpgrad.util import argsort
 from shrimpgrad.future import FutureTensor
 
 OptionalGradients: TypeAlias = Tuple[Optional[FutureTensor], ...]
@@ -100,19 +98,19 @@ class ReLU(Function):
   @staticmethod
   def forward(ctx: FunctionContext, x: FutureTensor) -> FutureTensor:
     ctx.save_for_backward(x)
-    return PythonRuntime.exec(BinaryOps.MAX, x, FutureTensor.zeros_like(x))
+    return x.alu(BinaryOps.MAX, FutureTensor.zeros_like(x))
   @staticmethod
   def backward(ctx: FunctionContext, grad_out: FutureTensor) -> FutureTensor:
     x = ctx.saved_tensors[0]
     # dx' = 0 if x < else 1
-    dx = PythonRuntime.exec(BinaryOps.CMPLT, FutureTensor.zeros_like(x), x)
-    return PythonRuntime.exec(BinaryOps.MUL, dx, grad_out) 
+    dx = FutureTensor.zeros_like(x).alu(BinaryOps.CMPLT, x)
+    return dx.alu(BinaryOps.MUL, grad_out) 
 
 class Sum(Function):
   @staticmethod
   def forward(ctx: FunctionContext, x: FutureTensor, axis: Tuple[int,...]=(0,), keepdim=False) -> FutureTensor:
     ctx.save_for_backward(x)
-    return PythonRuntime.exec(ReduceOps.SUM, x, ax=axis, keepdim=keepdim) 
+    return x.reduce(ReduceOps.SUM, ax=axis, keepdim=keepdim) 
   @staticmethod
   def backward(ctx: FunctionContext, grad_out: FutureTensor) -> FutureTensor:
     x = ctx.saved_tensors[0]
@@ -124,26 +122,18 @@ class Log(Function):
     ctx.save_for_backward(x)
     # ln(x) = log2(x)/log2(e) <by change of base>
     #       = log2(x) / (1/ln(2)) = log2(x) * ln(2)
-    return PythonRuntime.exec(BinaryOps.MUL, PythonRuntime.exec(UnaryOps.LOG2, x), x.const(math.log(2)))
+    return x.alu(UnaryOps.LOG2).alu(BinaryOps.MUL, x.const(math.log(2)))
   @staticmethod
   def backward(ctx: FunctionContext, grad_out: FutureTensor) -> FutureTensor:
     # dL/dx = 1/x * grad_out
     x = ctx.saved_tensors[0]
-    return PythonRuntime.exec(BinaryOps.DIV, grad_out, x)
+    return grad_out.alu(BinaryOps.DIV, x)
 
 class Reshape(Function):
   @staticmethod
   def forward(ctx: FunctionContext, x: FutureTensor, shape: Tuple[int,...] ) -> FutureTensor:
     ctx.save_for_backward(x)
-    if prod(shape) != x.numel: raise RuntimeError(f'shape \'{shape}\' is invalid for input of size {x.numel}')
-    if x.contiguous:
-      if shape == x.shape:
-        return FutureTensor(shape, x.data, dtype=x.dtype)
-      # Reshape from scalar to n-dim tensor 
-      if x.is_scalar() and len(shape):
-        return FutureTensor(shape, [x.data], dtype=x.dtype)
-      return FutureTensor(shape, x.data if len(shape) else x.data[0], dtype=x.dtype)
-    return FutureTensor(shape, flatten(x), dtype=x.dtype)
+    return x.reshape(shape)
   @staticmethod
   def backward(ctx: FunctionContext, grad_out: FutureTensor) -> FutureTensor:
     x = ctx.saved_tensors[0] 
@@ -154,12 +144,7 @@ class Permute(Function):
   def forward(ctx: FunctionContext, x: FutureTensor, order: Tuple[int, ...]) -> FutureTensor:
     ctx.save_for_backward(x)
     ctx.order = order
-    new_shape = [x.shape[i] for i in order]
-    new_strides = [x.strides[i] for i in order]
-    out = FutureTensor(tuple(new_shape), x.data, dtype=x.dtype) 
-    out.strides = new_strides
-    out.contiguous = False
-    return out 
+    return x.permute(order) 
   @staticmethod 
   def backward(ctx: FunctionContext, grad_out: FutureTensor) -> FutureTensor:
     return grad_out.permute(argsort(ctx.order))
@@ -169,14 +154,7 @@ class Expand(Function):
   def forward(ctx: FunctionContext, x: FutureTensor, shape: Tuple[int, ...]) -> FutureTensor:
     ctx.save_for_backward(x)
     ctx.expanded_axis = []
-    out = FutureTensor.zeros_like(x)
-    for i, (si, so) in enumerate(zip(x.shape, shape)):
-      if si != so: 
-        out.strides[i] = 0
-        ctx.expanded_axis.append(i)
-    out.shape = shape
-    out.data = x.data
-    return out
+    return x.expand(shape) 
   @staticmethod
   def backward(ctx: FunctionContext, grad_out: FutureTensor) -> FutureTensor:
     return grad_out.sum(axis=tuple(ctx.expanded_axis)) 
@@ -184,14 +162,14 @@ class Expand(Function):
 class Less(Function):
   @staticmethod
   def forward(ctx: FunctionContext, x:FutureTensor, y:FutureTensor) -> FutureTensor: 
-    return PythonRuntime.exec(BinaryOps.CMPLT, x, y)
+    return x.alu(BinaryOps.CMPLT, y)
   @staticmethod
   def backward(ctx: FunctionContext, grad_out:FutureTensor) -> OptionalGradients: return None, None
 
 class Eq(Function):
   @staticmethod
   def forward(ctx: FunctionContext, x:FutureTensor, y:FutureTensor) -> FutureTensor:
-    return PythonRuntime.exec(BinaryOps.CMPEQ, x, y)
+    return x.alu(BinaryOps.CMPEQ, y)
   @staticmethod
   def backward(ctx: FunctionContext, grad_out:FutureTensor) -> OptionalGradients: return None, None
 
@@ -200,34 +178,33 @@ class Cast(Function):
   def forward(ctx: FunctionContext, x:FutureTensor, dtype: shrimp.DType) -> FutureTensor:
     ctx.save_for_backward(x)
     ctx.input_dtype = x.dtype
-    x.data = list(map(functools.partial(dtypes.cast, dtype), x.data)) if not x.is_scalar() else dtypes.cast(dtype, x.data)
     return x 
   @staticmethod
   def backward(ctx: FunctionContext, grad_out:FutureTensor) -> FutureTensor:
-    grad_out.data = map(functools.partial(dtypes.cast, ctx.input_dtype), grad_out.data)
-    return grad_out
+    # grad_out.data = map(functools.partial(dtypes.cast, ctx.input_dtype), grad_out.data)
+    return grad_out.cast(ctx.input_dtype)
 
 class Where(Function):
   @staticmethod
   def forward(ctx: FunctionContext, condition: FutureTensor, x: FutureTensor, y: FutureTensor) -> FutureTensor:
     ctx.save_for_backward(x, y)
     ctx.cond = condition
-    return PythonRuntime.exec(TernaryOps.WHERE, condition, x, y)
+    return condition.alu(TernaryOps.WHERE, x, y)
   @staticmethod
   def backward(ctx: FunctionContext, grad_out: FutureTensor) -> OptionalGradients:
     cond = ctx.cond
-    return PythonRuntime.exec(TernaryOps.WHERE, cond, grad_out, grad_out.const(0.0)), \
-      PythonRuntime.exec(TernaryOps.WHERE, cond, grad_out.const(0.0), grad_out) 
+    return cond.alu(TernaryOps.WHERE, grad_out, grad_out.const(0.0)), \
+      cond.alu(TernaryOps.WHERE, grad_out.const(0.0), grad_out) 
 
 class Sigmoid(Function):
   @staticmethod
   def forward(ctx: FunctionContext, x: FutureTensor) -> FutureTensor:
     ctx.save_for_backward(x)
-    x_ = PythonRuntime.exec(BinaryOps.MUL, x, x.const(-1.0/math.log(2)))
-    x_ = PythonRuntime.exec(UnaryOps.EXP2, x_)
-    x_ = PythonRuntime.exec(BinaryOps.ADD, x_.const(1.0), x_)
-    ctx.ret = PythonRuntime.exec(BinaryOps.DIV, x_.const(1.0), x_)
+    a = x.alu(BinaryOps.MUL, x.const(-1.0/math.log(2))).alu(UnaryOps.EXP2)
+    b = a.const((1.0)).alu(BinaryOps.ADD, a)
+    c = b.const(1.0).alu(BinaryOps.ADD, b)
+    ctx.ret = c.const(1.0).alu(BinaryOps.DIV, c) 
     return ctx.ret
   @staticmethod
   def backward(ctx: FunctionContext, grad_out: FutureTensor) -> FutureTensor:
-    return PythonRuntime.exec(BinaryOps.MUL, PythonRuntime.exec(BinaryOps.MUL, ctx.ret, PythonRuntime.exec(BinaryOps.SUB, ctx.ret.const(1.0), ctx.ret)), grad_out)
+    return ctx.ret.alu(BinaryOps.MUL, ctx.ret.const(1).alu(BinaryOps.SUB, ctx.ret)).alu(BinaryOps.MUL, grad_out)
--- shrimpgrad/future.py
+++ shrimpgrad/future.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
-from typing import Any, List, Optional, Tuple, Union
+from typing import Any, List, Tuple, Union
 from shrimpgrad.dtype import ConstType, DType
 from shrimpgrad.runtime.ops import BinaryOps, BufferOps, LoadOps, Op, ReduceOps, TernaryOps, UnaryOps
-from shrimpgrad.tensor import Tensor 
+from shrimpgrad.tensor import Tensor
+from shrimpgrad.memory.buffer import Buffer, Allocator
+
+def create_dependent_future_tensor(): pass
+def create_independent_future_tensor(): pass
 
 class FutureTensor:
   """An unresolved tensor described by future computations. 
@@ -25,6 +29,40 @@ class FutureTensor:
     pass
   
   def const(self, val:ConstType) -> FutureTensor:
+    pass
+  
+  def reshape(self, shape: Tuple[int,...]):
+    # if prod(shape) != x.numel: raise RuntimeError(f'shape \'{shape}\' is invalid for input of size {x.numel}')
+    # if x.contiguous:
+    #   if shape == x.shape:
+    #     return FutureTensor(shape, x.data, dtype=x.dtype)
+    #   # Reshape from scalar to n-dim tensor 
+    #   if x.is_scalar() and len(shape):
+    #     return FutureTensor(shape, [x.data], dtype=x.dtype)
+    #   return FutureTensor(shape, x.data if len(shape) else x.data[0], dtype=x.dtype)
+    # return FutureTensor(shape, flatten(x), dtype=x.dtype)
+    pass
+
+  def permute(self, order:Tuple[int,...]) -> FutureTensor:
+    # new_shape = [x._shape[i] for i in order]
+    # new_strides = [x._strides[i] for i in order]
+    # out = FutureTensor(tuple(new_shape), x._data, dtype=x._dtype) 
+    # out.strides = new_strides
+    # out.contiguous = False
+    pass
+
+  def expand(self, shape: Tuple[int,...]) -> FutureTensor:
+    # out = FutureTensor.zeros_like(x)
+    # for i, (si, so) in enumerate(zip(x.shape, shape)):
+    #   if si != so: 
+    #     out.strides[i] = 0
+    #     ctx.expanded_axis.append(i)
+    # out.shape = shape
+    # out.data = x.data
+    pass
+
+  def cast(self, dtype: DType) -> FutureTensor:
+    #  x.data = list(map(functools.partial(dtypes.cast, dtype), x.data)) if not x.is_scalar() else dtypes.cast(dtype, x.data)
     pass
 
   def resolve(self) -> Tensor: 
