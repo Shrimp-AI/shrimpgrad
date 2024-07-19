35b1a732a97ebdf3da6f67487663613a90bf73ba
Kevin Kenyon <kvkenyon@gmail.com>
2024-07-18 23:41:39 -0500

    [2/n] Add rendering of symbolic expressions

--- shrimpgrad/symbolic.py
+++ shrimpgrad/symbolic.py
@@ -1,50 +1,120 @@
 from __future__ import annotations
 from enum import Enum, auto
-from typing import Optional, TypeAlias
+from typing import Optional, Tuple, TypeAlias
 from dataclasses import dataclass
 
 @dataclass
 class Interval: 
-  start:symbolicint 
-  end: symbolicint 
+  start:sint
+  end: sint
 
-
-class Show:
-  def show(self) -> str:
-    raise NotImplementedError('implement show')
-
-class ArithOp(Show, Enum):
-  PLUS=auto()
+class ArithOp(Enum):
+  MUL=auto(); DIV=auto(); MOD=auto(); NEG=auto();
+  PLUS=auto(); SUB=auto(); 
+  AND=auto(); OR=auto();
+  LT=auto(); GT=auto(); LTE=auto(); GTE=auto(); EQ=auto()
   def show(self):
-    if self.name == 'PLUS': return "+"
-  def __str__(self):
-    return self.name
+    if self == self.PLUS: return "+"
+    elif self == self.MUL: return "*"
+    elif self == self.SUB: return "-"
+    elif self == self.DIV: return  "/"
+    elif self == self.MOD: return "%"
+    elif self == self.LT: return "<"
+    elif self == self.GT: return ">"
+    elif self == self.LTE: return "<="
+    elif self == self.GTE: return ">="
+    elif self == self.EQ: return "="
+    elif self == self.AND: return "&&"
+    elif self == self.NEG: return "-"
+    else: return "||"
 
-class Expr(Show):
-  pass
+precedence = {
+  ArithOp.MUL: 4, ArithOp.DIV: 4, ArithOp.MOD: 4,
+  ArithOp.PLUS: 3,ArithOp.SUB: 3,
+  ArithOp.AND: 2, ArithOp.OR: 2,
+  ArithOp.LT: 1, ArithOp.GT: 1, ArithOp.LTE: 1, ArithOp.GTE: 1, ArithOp.EQ: 1
+}
+
+class Expr:
+  def __add__(self, other: Expr): return Bin(ArithOp.PLUS, self, other)  
+  def __radd__(self, other): return self + other 
+  def __mul__(self, other: Expr): return Bin(ArithOp.MUL, self, other)
+  def __rmul__(self, other: Expr): return self * other 
+  def __sub__(self, other: Expr): return Bin(ArithOp.SUB, self, other) 
+  def __truediv__(self, other: Expr): return Bin(ArithOp.DIV, self, other) 
+  def __rtruediv__(self, other): return self /other 
+  def __mod__(self, other: Expr): return Bin(ArithOp.MOD, self, other)
+  def __rmod__(self, other: Expr): return self % other 
+  def __lt__(self, other: Expr): return Bin(ArithOp.LT, self, other)
+  def __lte__(self, other: Expr): return Bin(ArithOp.LTE, self, other)
+  def __gt__(self, other: Expr): return Bin(ArithOp.GT, self, other)
+  def __gte__(self, other: Expr): return Bin(ArithOp.GTE, self, other)
+  def __eq__(self, other: Expr): return Bin(ArithOp.EQ, self, other)
+  def __neg__(self): return Unary(ArithOp.NEG, self) 
+  def ifelse(self, x, y): return IfElse(self, x , y)
+  def and_(self, x): return Bin(ArithOp.AND, self, x)
+  def or_(self, x): return Bin(ArithOp.OR, self, x)
+
+def symbols(syms: str) -> Tuple[sym,...]:
+  return tuple([Symbol(x) for x in syms.split(',')])
 
 class Symbol(Expr):
+  __match_args__ = ('name', )
   def __init__(self, name:str, domain: Optional[Interval]=None):
     self.name: str = name
     self.domain: Optional[Interval] = domain
-  def __add__(self, other: Symbol):
-    return Bin(ArithOp.PLUS, self, other)  
-  def __repr__(self):
-    return self.name
-  def __str__(self):
-    return self.name
+  def __repr__(self): return f"(Sym {self.name} {self.domain})" 
+  def __str__(self): return self.name
 
 class Lit(Expr):
+  __match_args__ = ('val', )
   def __init__(self, val:int): self.val = val
   def __repr__(self): return f"(Lit {self.val})"
-  def __str__(self): return str(self.val)
+  def __str__(self): return f"{self.val}"
 
 class Bin(Expr):
+  __match_args__ = ('op', 'lhs', 'rhs')
   def __init__(self, op, lhs: Expr, rhs: Expr):
     self.op, self.lhs, self.rhs = op, lhs, rhs
+  def precedence(self) -> int: return precedence[self.op] 
   def __repr__(self):
     return f"(Bin {self.op} {self.lhs} {self.rhs})"
   def __str__(self):
     return f"{self.lhs} {self.op.show()} {self.rhs}"
 
-symbolicint: TypeAlias = Symbol|int
+class Unary(Expr):
+  __match_args__ = ('op', 'arg')
+  def __init__(self, op, arg: Expr):
+    self.op, self.arg = op, arg 
+  def __repr__(self):
+    return f"(Unary {self.op} {self.arg})"
+  def __str__(self):
+    return f"{self.op.show()}{self.arg}"
+
+class IfElse(Expr):
+  __match_args__ = ('cond', 'a', 'b')
+  def __init__(self, cond: Expr, a: Expr, b: Expr):
+    self.cond, self.a, self.b = cond, a, b
+  def __repr__(self):
+    return f"(IfElse {self.cond} {self.a} {self.b})"
+  def __str__(self):
+    return f"{self.cond} ? {self.a} : {self.b}"
+
+def render(expr: Expr, prec:int=0) -> str:
+  match expr:
+    case Lit(val):
+      return f"{val}" 
+    case Symbol(name):
+      return f"{name}" 
+    case Bin(op, lhs, rhs):
+      res = f"{render(lhs, expr.precedence())} {op.show()} {render(rhs, expr.precedence() + 1)}"
+      return res if expr.precedence() > prec else f"({res})"
+    case Unary(op, lhs):
+      return f"{op.show()}{render(lhs)}"
+    case IfElse(cond, a, b):
+      return f"{render(cond)} ? {render(a)} : {render(b)}"
+    case _:
+      raise ValueError(f"Invalid expression value: {repr(expr)}")
+
+sint: TypeAlias = Symbol|int
+sym: TypeAlias = Symbol
--- test/test_symbolic.py
+++ test/test_symbolic.py
@@ -1,6 +1,6 @@
 import unittest
 
-from shrimpgrad.symbolic import ArithOp, Bin, Lit, Symbol, Interval
+from shrimpgrad.symbolic import ArithOp, Bin, Lit, Symbol, Interval, render, symbols, precedence
 from shrimpgrad.view import ViewTracker
 
 class TestSymbolic(unittest.TestCase):
@@ -22,12 +22,19 @@ class TestSymbolic(unittest.TestCase):
       assert idx.domain.start == 0 and idx.domain.end == view.shape[i]
       assert idx.name == f"idx{i}"
   
+  def test_symbols(self):
+    x,y,z,w = symbols('x,y,z,w')
+    assert x.name == "x"
+    assert y.name == "y"
+    assert z.name == "z"
+    assert w.name == "w"
+  
   def test_literal_expr(self):
     lit = Lit(4) 
     assert str(lit) == "4", "literal is not correct"
   
   def test_arith_op(self):
-    assert ArithOp.PLUS.show() == "+"
+    assert str(ArithOp.PLUS.show()) == "+"
   
   def test_binary_expr(self):
     bin = Bin(ArithOp.PLUS, Lit(4), Lit(5))
@@ -44,3 +51,52 @@ class TestSymbolic(unittest.TestCase):
     y = Symbol("y")
     z = x + y
     assert "x + y" == str(z), "wrong str" 
+  
+  def test_op_precedence(self):
+    assert precedence[ArithOp.PLUS] == precedence[ArithOp.SUB]
+    assert precedence[ArithOp.PLUS] < precedence[ArithOp.MUL]
+    assert precedence[ArithOp.MOD] > precedence[ArithOp.AND]
+  
+  def test_unary_op(self):
+    x = Symbol("x")
+    expr = -x
+    assert str(expr) == "-x"
+
+  def test_render_expr(self):
+    w,x,y,z = symbols("w,x,y,z") 
+    expr = w + x - z * w / y % z 
+    assert "(w + x) - (((z * w) / y) % z)" == render(expr)
+  
+  def test_render_unary_binary(self):
+    x,y,z = symbols("x,y,z")
+    expr = x * y + (-z)
+    assert "x * y + -z" == render(expr)
+  
+  def test_render_ifelse(self):
+    x,y,z = symbols("x,y,z")
+    expr = x.ifelse(y,z)
+    assert "x ? y : z", render(expr)
+  
+  def test_render_complex(self):
+    w,x,y,z = symbols("w,x,y,z") 
+    expr = (w*y).ifelse((-x)%z, (y/w).ifelse(x,y)) 
+    assert "w * y ? -x % z : y / w ? x : y" == render(expr)
+  
+  def test_render_pad(self):
+    shape = (4,4)
+    stride = (2,1)
+    offset = -3
+    idx0, idx1 = Symbol("idx0", Interval(0, shape[0])), \
+      Symbol("idx1", Interval(0, shape[1]))
+    off = Lit(offset)
+    strides = [Lit(st) for st in stride]
+    iexpr = idx0*strides[0]+off+idx1*strides[1]
+    assert "(idx0 * 2 + -3) + (idx1 * 1)", render(iexpr)
+    a = idx0*(Lit(-1)) > Lit(0) 
+    b = idx1*(Lit(-1)) > Lit(0)
+    c = idx0 < Lit(3)
+    d = idx1 < Lit(3)
+    vexpr = a.and_(b.and_(c).and_(d)) 
+    assert "(idx0 * -1 > 0) && (((idx1 * -1 > 0) && (idx0 < 3)) && (idx1 < 3))", render(vexpr)
+    
+
